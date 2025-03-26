59a452a8dca25c7d16be73df9ab1142d4a89713c
Drylegend <130828463+Drylegend@users.noreply.github.com>
2025-03-26 19:29:51 +0530

    Update Controller.tsx

--- frontend/src/Components/Controller.tsx
+++ frontend/src/Components/Controller.tsx
@@ -27,7 +27,7 @@ const Controller = () => {
         formData.append("file", blob, "myFile.wav");
 
         await axios
-          .post("https://speakapp-production-1a3a.up.railway.app/post-audio", formData, {
+          .post(`${import.meta.env.VITE_BACKEND_URL}/post-audio`, formData, {
             headers: {
               "Content-Type": "audio/mpeg",
             },
